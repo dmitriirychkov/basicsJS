81325920e1c694ace976fb5dbf808b767baa2086
dmitriirychkov <dmitriirychkov@yandex.ru>
2025-02-18 20:22:01 +0500

    Добавлен src/main.js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+console.log(greet('World')); // Вывод: Hello, World!
+
+const farewell = (name) => {
+    return `Farewell, ${name}!`;
+};
+console.log(farewell('World')); // Вывод: Farewell, World!
